2fa1dc194049d16d5453aaad49255a695cc19120
siddsoundar <youremail@example.com>
2023-10-01 22:40:00 -0400

    auth module

--- server/src/controllers/users.ts
+++ server/src/controllers/users.ts
@@ -4,6 +4,7 @@ import { UserDocument } from "../types/user.interface";
 import { Error } from "mongoose";
 import jwt from "jsonwebtoken";
 import { secret } from "../config";
+import { ExpressRequestInterface } from "../types/expressRequest.interface";
 
 const normalizeUser = (user: UserDocument) => {
     const token = jwt.sign({ id: user.id, email: user.email }, secret);
@@ -62,4 +63,11 @@ export const login = async (
     } catch (err) {
         next(err);
     }
+};
+
+export const currentUser = (req: ExpressRequestInterface, res: Response) => {
+    if (!req.user) {
+        return res.sendStatus(401);
+    }
+    res.send(normalizeUser(req.user));
 };
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import { Server } from "socket.io"
 import mongoose from 'mongoose'
 import * as usersController from './controllers/users'
 import bodyParser from 'body-parser';
+import authMiddleware from './middlewares/auth'
 
 const app = express();
 const httpServer = createServer(app);
@@ -21,6 +22,7 @@ app.post('/api/users', usersController.register);
 
 app.post('/api/users/login', usersController.login);
 
+app.get('/api/user', authMiddleware, usersController.currentUser)
 
 io.on('connection', () => {
     console.log("connect")
